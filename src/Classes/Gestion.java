051b286ab202afcdae8a74580af066e75860e46c
mansour.m <m.mansourjamaleddine@condorcet.be>
2023-03-02 13:35:17 +0100

    amelioration lors de la creation des ouvrages avec une métode ajoutAuteur pour ajouter les auteurs a l'ouvrage + autres petites modifications

--- src/Classes/Gestion.java
+++ src/Classes/Gestion.java
@@ -12,25 +12,25 @@ public class Gestion {
 
     private static List<Auteur> laut = new ArrayList<>();
     private static List<Lecteur> llect = new ArrayList<>();
-    private static List<Ouvrage> louv= new ArrayList<>();
+    private static List<Ouvrage> louv = new ArrayList<>();
     private static List<Exemplaire> lex = new ArrayList<>();
-    private static List<Rayon> lrayon= new ArrayList<>();
+    private static List<Rayon> lrayon = new ArrayList<>();
     private static List<Location> lloc = new ArrayList<>();
 
 
-    public void populate(){
-        Auteur a = new Auteur("Verne","Jules","France");
+    public void populate() {
+        Auteur a = new Auteur("Verne", "Jules", "France");
         laut.add(a);
 
-        Livre l = new Livre("Vingt mille lieues sous les mers",10, LocalDate.of(1880,1,1),1.50,"français","aventure","a125",350,TypeLivre.ROMAN,"histoire de sous-marin");
+        Livre l = new Livre("Vingt mille lieues sous les mers", 10, LocalDate.of(1880, 1, 1), 1.50, "français", "aventure", "a125", 350, TypeLivre.ROMAN, "histoire de sous-marin");
         louv.add(l);
 
         a.addOuvrage(l);
 
-        a = new Auteur("Spielberg","Steven","USA");
+        a = new Auteur("Spielberg", "Steven", "USA");
         laut.add(a);
 
-        DVD d = new DVD("AI",12,LocalDate.of(2000,10,1),2.50,"anglais","SF",4578l,"120 min",(byte)2);
+        DVD d = new DVD("AI", 12, LocalDate.of(2000, 10, 1), 2.50, "anglais", "SF", 4578l, "120 min", (byte) 2);
         d.getAutresLangues().add("français");
         d.getAutresLangues().add("italien");
         d.getSousTitres().add("néerlandais");
@@ -38,52 +38,52 @@ public class Gestion {
 
         a.addOuvrage(d);
 
-        a = new Auteur("Kubrick","Stanley","GB");
+        a = new Auteur("Kubrick", "Stanley", "GB");
         laut.add(a);
 
         a.addOuvrage(d);
 
 
-        CD c = new CD("The Compil 2023",0,LocalDate.of(2023,1,1),2,"English","POP",1245,(byte)20,"100 min");
+        CD c = new CD("The Compil 2023", 0, LocalDate.of(2023, 1, 1), 2, "English", "POP", 1245, (byte) 20, "100 min");
         louv.add(c);
 
-        Rayon r = new Rayon("r12","aventure");
+        Rayon r = new Rayon("r12", "aventure");
         lrayon.add(r);
 
-        Exemplaire e = new Exemplaire("m12","état neuf",l);
+        Exemplaire e = new Exemplaire("m12", "état neuf", l);
         lex.add(e);
         e.setRayon(r);
 
 
-        r = new Rayon("r45","science fiction");
+        r = new Rayon("r45", "science fiction");
         lrayon.add(r);
 
-        e = new Exemplaire("d12","griffé",d);
+        e = new Exemplaire("d12", "griffé", d);
         lex.add(e);
 
         e.setRayon(r);
 
 
-        Lecteur lec = new Lecteur(1,"Dupont","Jean",LocalDate.of(2000,1,4),"Mons","jean.dupont@mail.com","0458774411");
+        Lecteur lec = new Lecteur(1, "Dupont", "Jean", LocalDate.of(2000, 1, 4), "Mons", "jean.dupont@mail.com", "0458774411");
         llect.add(lec);
 
-        Location loc = new Location(LocalDate.of(2023,2,1),LocalDate.of(2023,3,1),lec,e);
+        Location loc = new Location(LocalDate.of(2023, 2, 1), LocalDate.of(2023, 3, 1), lec, e);
         lloc.add(loc);
-        loc.setDateRestitution(LocalDate.of(2023,2,4));
+        loc.setDateRestitution(LocalDate.of(2023, 2, 4));
 
-        lec = new Lecteur(1,"Durant","Aline",LocalDate.of(1980,10,10),"Binche","aline.durant@mail.com","045874444");
+        lec = new Lecteur(1, "Durant", "Aline", LocalDate.of(1980, 10, 10), "Binche", "aline.durant@mail.com", "045874444");
         llect.add(lec);
 
-        loc = new Location(LocalDate.of(2023,2,5),LocalDate.of(2023,3,5),lec,e);
+        loc = new Location(LocalDate.of(2023, 2, 5), LocalDate.of(2023, 3, 5), lec, e);
         lloc.add(loc);
     }
 
     private void menu() {
-        List options = new ArrayList<>(Arrays.asList("Auteurs","Ouvrages","Exemplaires","Rayons","Lecteurs","Locations","Affichage","Fin"));
+        List options = new ArrayList<>(Arrays.asList("Auteurs", "Ouvrages", "Exemplaires", "Rayons", "Lecteurs", "Locations", "Affichage", "Fin"));
 
-        do{
-            for(int i=0;i<options.size();i++){
-                System.out.println((i+1)+"."+options.get(i));
+        do {
+            for (int i = 0; i < options.size(); i++) {
+                System.out.println((i + 1) + "." + options.get(i));
             }
 
             int choix;
@@ -91,18 +91,33 @@ public class Gestion {
                 System.out.println("choix :");
                 choix = sc.nextInt();
                 sc.skip("\n");
-            } while(choix <1 || choix > options.size());
-            switch (choix){
-                case 1 :gestAuteurs(); break;
-                case 2 : gestOuvrages();break;
-                case 3 : gestExemplaires();break;
-                case 4 : gestRayons();break;
-                case 5 : gestLecteurs();break;
-                case 6 : gestLocations();break;
-                case 7 : testaffichage();break;
-                default:System.exit(0);
+            } while (choix < 1 || choix > options.size());
+            switch (choix) {
+                case 1:
+                    gestAuteurs();
+                    break;
+                case 2:
+                    gestOuvrages();
+                    break;
+                case 3:
+                    gestExemplaires();
+                    break;
+                case 4:
+                    gestRayons();
+                    break;
+                case 5:
+                    gestLecteurs();
+                    break;
+                case 6:
+                    gestLocations();
+                    break;
+                case 7:
+                    testaffichage();
+                    break;
+                default:
+                    System.exit(0);
             }
-        }  while (true);
+        } while (true);
     }
 
     private void gestLocations() {
@@ -112,46 +127,46 @@ public class Gestion {
 
     private void gestLecteurs() {
         System.out.println("numéro");
-        int num=sc.nextInt();
+        int num = sc.nextInt();
         sc.skip("\n");
         System.out.println("nom ");
-        String nom=sc.nextLine();
+        String nom = sc.nextLine();
         System.out.println("prénom ");
-        String prenom=sc.nextLine();
+        String prenom = sc.nextLine();
         System.out.println("date de naissance");
         String[] jma = sc.nextLine().split(" ");
         int j = Integer.parseInt(jma[0]);
         int m = Integer.parseInt(jma[1]);
         int a = Integer.parseInt(jma[2]);
-        LocalDate dn= LocalDate.of(a,m,j);
+        LocalDate dn = LocalDate.of(a, m, j);
         System.out.println("adresse");
-        String adr=sc.nextLine();
+        String adr = sc.nextLine();
         System.out.println("mail");
-        String mail=sc.nextLine();
+        String mail = sc.nextLine();
         System.out.println("tel ");
-        String tel=sc.nextLine();
-        Lecteur lect = new Lecteur(num,nom,prenom,dn,adr,mail,tel);
+        String tel = sc.nextLine();
+        Lecteur lect = new Lecteur(num, nom, prenom, dn, adr, mail, tel);
         llect.add(lect);
         System.out.println("lecteur créé");
     }
 
     private void gestRayons() {
         System.out.println("code ");
-        String code=sc.next();
+        String code = sc.next();
         System.out.println("genre ");
-        String genre=sc.next();
-        Rayon r = new Rayon(code,genre);
+        String genre = sc.next();
+        Rayon r = new Rayon(code, genre);
         System.out.println("rayon créé");
     }
 
     private void gestExemplaires() {
         System.out.println("matricule ");
-        String mat=sc.next();
+        String mat = sc.next();
         System.out.println("etat  ");
-        String etat=sc.next();
+        String etat = sc.next();
         System.out.println("ouvrage ");
-        for(int i=0;i<louv.size();i++){
-            System.out.println((i+1+"."+louv.get(i)));
+        for (int i = 0; i < louv.size(); i++) {
+            System.out.println((i + 1 + "." + louv.get(i)));
 
         }
         int choix;
@@ -159,8 +174,8 @@ public class Gestion {
             System.out.println("choix :");
             choix = sc.nextInt();
             sc.skip("\n");
-        } while(choix <1 || choix > louv.size());
-        Exemplaire ex = new Exemplaire(mat,etat,louv.get(choix-1));
+        } while (choix < 1 || choix > louv.size());
+        Exemplaire ex = new Exemplaire(mat, etat, louv.get(choix - 1));
         lex.add(ex);
         System.out.println("exemplaire créé");
     }
@@ -168,27 +183,27 @@ public class Gestion {
     private void gestOuvrages() {
         Ouvrage o = null;
         System.out.println("titre");
-        String titre= sc.nextLine();
+        String titre = sc.nextLine();
         System.out.println("age minimum");
-        int ageMin= sc.nextInt();
+        int ageMin = sc.nextInt();
         sc.skip("\n");
         System.out.println("date de parution");
         String[] jma = sc.nextLine().split(" ");
         int j = Integer.parseInt(jma[0]);
         int m = Integer.parseInt(jma[1]);
         int a = Integer.parseInt(jma[2]);
-        LocalDate dp= LocalDate.of(a,m,j);
+        LocalDate dp = LocalDate.of(a, m, j);
         System.out.println("prix de location");
         double ploc = sc.nextDouble();
         sc.skip("\n");
         System.out.println("langue");
-        String langue=sc.nextLine();
+        String langue = sc.nextLine();
         System.out.println("genre");
-        String genre=sc.nextLine();
+        String genre = sc.nextLine();
         TypeOuvrage[] to = TypeOuvrage.values();
 
-        for(int i =0;i<to.length;i++){
-            System.out.println((i+1)+"."+to[i]);
+        for (int i = 0; i < to.length; i++) {
+            System.out.println((i + 1) + "." + to[i]);
         }
 
         int choix;
@@ -196,106 +211,123 @@ public class Gestion {
             System.out.println("choix :");
             choix = sc.nextInt();
             sc.skip("\n");
-        } while(choix <1 || choix > to.length);
-        switch (choix){
-            case 1 :
+        } while (choix < 1 || choix > to.length);
+        switch (choix) {
+            case 1:
                 System.out.println("isbn ");
                 String isbn = sc.next();
                 System.out.println("pages ");
                 int nbrePages = sc.nextInt();
                 sc.skip("\n");
-                TypeLivre tl= TypeLivre.ROMAN;
+                TypeLivre tl = TypeLivre.ROMAN;
                 String resume = "résumé du livre";
-                o=new Livre(titre,ageMin,dp,ploc,langue,genre,isbn,nbrePages,tl,resume);
-                ;break;
-            case 2 :
+                o = new Livre(titre, ageMin, dp, ploc, langue, genre, isbn, nbrePages, tl, resume);
+                louv.add(o);
+                ajoutAuteur(o);
+                break;
+            case 2:
                 System.out.println("code : ");
-                long codeCD =sc.nextLong();
+                long codeCD = sc.nextLong();
                 System.out.println("nombre plages : ");
                 byte nbrePlages = sc.nextByte();
                 sc.skip("\n");
                 System.out.println("durée totale : ");
-                String dureetotaleCD=sc.nextLine();
-                o=new CD(titre,ageMin,dp,ploc,langue,genre,codeCD,nbrePlages,dureetotaleCD);
+                String dureetotaleCD = sc.nextLine();
+                o = new CD(titre, ageMin, dp, ploc, langue, genre, codeCD, nbrePlages, dureetotaleCD);
+                louv.add(o);
 
-                ;break;
-            case 3 :
-                int i;
-                int choix1;
+                ajoutAuteur(o);
+
+                break;
+            case 3:
                 System.out.println("code : ");
-                long code =sc.nextLong();
+                long code = sc.nextLong();
                 sc.skip("\n");
                 System.out.println("durée totale : ");
-                String dureetotale=sc.nextLine();
+                String dureetotale = sc.nextLine();
                 System.out.println("nombre bonus : ");
                 byte nbreBonus = sc.nextByte();
+                sc.skip("\n");
 
-                DVD d = new DVD(titre,ageMin,dp,ploc,langue,genre,code,dureetotale,nbreBonus);
+                DVD d = new DVD(titre, ageMin, dp, ploc, langue, genre, code, dureetotale, nbreBonus);
                 d.getAutresLangues().add("italien");
                 d.getSousTitres().add("néerlandais");
-                o=d;
-
-                // code pour essayer de donner a l'ouvrage , un auteur 
-                System.out.println("Liste des auteurs existants :");
-                for(j = 0; j < laut.size(); j++) {
-                    System.out.println((j+1) + ". " + laut.get(j).getNom());
-                }
-
-                ArrayList<Auteur> listeAuteurs = new ArrayList<Auteur>();
-                int choix2;
-                do {
-                    System.out.println("Choisissez un auteur pour l'ouvrage, ou tapez \"Fin\" pour terminer :");
-                    String choixAuteur = sc.nextLine();
-                    if (choixAuteur.equalsIgnoreCase("Fin")) {
-                        break;
-                    }
-                    choix2 = Integer.parseInt(choixAuteur);
-                    if (choix2 < 1 || choix2 > laut.size()) {
-                        System.out.println("Choix invalide. Veuillez réessayer.");
-                    } else {
-                        Auteur auteurChoisi = laut.get(choix2-1);
-                        listeAuteurs.add(auteurChoisi);
-                        System.out.println("Auteur ajouté : " + auteurChoisi.getNom());
-                    }
-                } while (true);
-
-                o.setLauteurs(listeAuteurs);
-
-                for (Auteur auteurChoisi : listeAuteurs) {
-                    auteurChoisi.getLouvrage().add(o);
-                }
+                louv.add(d);
+                ajoutAuteur(d);
 
 
-
-
-                ;break;
-
+                break;
         }
-        louv.add(o);
+
         System.out.println("ouvrage créé");
     }
 
     private void gestAuteurs() {
         System.out.println("nom ");
-        String nom=sc.nextLine();
+        String nom = sc.nextLine();
         System.out.println("prénom ");
-        String prenom=sc.nextLine();
+        String prenom = sc.nextLine();
         System.out.println("nationalité");
-        String nat=sc.nextLine();
-        Auteur a  = new Auteur(nom,prenom,nat);
+        String nat = sc.nextLine();
+        Auteur a = new Auteur(nom, prenom, nat);
         laut.add(a);
         System.out.println("écrivain créé");
     }
-public void testaffichage(){
 
-    for(int i = 0; i < laut.size(); i++) {
-        System.out.println((i+1) + "." + laut.get(i));
-        List<Ouvrage> ouvrages = laut.get(i).getLouvrage();
-        for(int j = 0; j < ouvrages.size(); j++) {
-            System.out.println("\t" + ouvrages.get(j));
+    public void testaffichage() {
+
+        System.out.println("Liste des ouvrages avec leurs auteurs :");
+        for (Ouvrage ouvrage : louv) {
+            System.out.println("Titre : " + ouvrage.getTitre());
+
+            System.out.println("Auteur(s) : ");
+            for (Auteur auteur : ouvrage.getLauteurs()) {
+                System.out.println("- " + auteur.getNom() + " " + auteur.getPrenom());
+            }
+
+            System.out.println();
         }
     }
-}
+
+    public void ajoutAuteur(Ouvrage o){
+        ArrayList<Auteur> auteursExistant = new ArrayList<Auteur>(laut);
+
+        int choixAuteur;
+        do {
+            System.out.println("Liste des auteurs existants :");
+            for (int j = 0; j < auteursExistant.size(); j++) {
+                System.out.println((j + 1) + ". " + auteursExistant.get(j).getNom());
+            }
+            System.out.println("Choisissez un auteur pour l'ouvrage, ou tapez \"0\" pour terminer :");
+            choixAuteur = sc.nextInt();
+            sc.skip("\n");
+            if (choixAuteur == 0) {
+                break;
+            } else if (choixAuteur < 0 || choixAuteur > auteursExistant.size()) {
+                System.out.println("Choix invalide. Veuillez réessayer.");
+            } else {
+                Auteur auteurChoisi = auteursExistant.get(choixAuteur - 1);
+
+                boolean auteurDejaPresent = false;// on cré'e un boolean pour la verification de l'auteur
+                for (Auteur auteur : o.getLauteurs()) {//parcoure la liste pour verifier si l'auteur appartient ou non à l'ouvrage en question
+                    if (auteur.getNom().equals(auteurChoisi.getNom())) {
+                        auteurDejaPresent = true;//si on trouve l'auteur le boolean devient true
+                        break;
+                    }
+                }
+
+                if (auteurDejaPresent) {
+                    System.out.println("Cet auteur est déjà associé à cet ouvrage. Veuillez en choisir un autre.");// si le boolean = true alors on affiche un message d'erreur
+                } else {
+                    auteurChoisi.addOuvrage(o);//sinon on rajoute l'auteur dans la liste d'auteurs de l'ouvrage
+                    System.out.println("Auteur ajouté : " + auteurChoisi.getNom());
+                }
+            }
+        } while (true);
+
+
+    }
+
     public static void main(String[] args) {
         Gestion g = new Gestion();
         g.populate();
