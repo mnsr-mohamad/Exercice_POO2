d35b8c3bff801450a496ff0a8d666163eab99f6f
mansour.m <m.mansourjamaleddine@condorcet.be>
2023-04-28 11:57:03 +0200

    Commit de la biblio du prof pour faire proprement les exercices en classe

---
+++ src/bibliotheque/mvp/model/ExemplaireModel.java
@@ -0,0 +1,49 @@
+package bibliotheque.mvp.model;
+
+import bibliotheque.metier.Exemplaire;
+import bibliotheque.metier.Lecteur;
+import bibliotheque.metier.Rayon;
+
+import java.util.List;
+
+public class ExemplaireModel extends AbstractModel<Exemplaire> implements SpecialExemplaire {
+    @Override
+    public void modifierEtat(Exemplaire ex, String etat) {
+         ex.setDescriptionEtat(etat);
+    }
+
+    @Override
+    public Lecteur lecteurActuel(Exemplaire ex) {
+        return ex.lecteurActuel();
+    }
+
+    @Override
+    public List<Lecteur> lecteurs(Exemplaire ex) {
+        return ex.lecteurs();
+    }
+
+    @Override
+    public void envoiMailLecteurActuel(Exemplaire ex) {
+     //TODO envoi mail lecteur
+    }
+
+    @Override
+    public void envoiMailLecteurs(Exemplaire ex) {
+     //TODO envoi mail lecteurs
+    }
+
+    @Override
+    public boolean enRetard(Exemplaire ex) {
+        return ex.enRetard();
+    }
+
+    @Override
+    public int joursRetard(Exemplaire ex) {
+        return ex.joursRetard();
+    }
+
+    @Override
+    public boolean enLocation(Exemplaire ex) {
+        return ex.enLocation();
+    }
+}
